8b6026778f76014c87d5e695a75ae7cb583b8c03
Lisa Kohls <lk210@hdm-stuttgart.de>
2022-04-20 17:20:29 +0200

    module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,5 @@ module SE2StartupProject {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens mainpackage to javafx.controls, javafx.graphics, javafx.fxml;
+    opens mainpackage to javafx.controls, javafx.fxml;
 }
