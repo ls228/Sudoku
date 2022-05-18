3775454f648342dadb975241a1a4cf37b5e56b91
Lisa Kohls <lk210@hdm-stuttgart.de>
2022-05-18 21:49:31 +0200

    Level

--- src/main/java/mainpackage/Controller.java
+++ src/main/java/mainpackage/Controller.java
@@ -2,21 +2,41 @@ package mainpackage;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
 
 public class Controller {
+    Stage window= new Stage();
 
+    public Button button;
+    @FXML
+    TextArea textArea;
+
+    @FXML
+    private Label label;
+
+    @FXML
+    protected void buttonPressed() {
+        String text = textArea.getText();
+        label.setText(text);
+        textArea.clear();
+    }
+    @FXML
+    protected void playPressed() {
+
+    }
+
+
+    /*
     @FXML
     private void sayHello(ActionEvent event) {
         System.out.println("Game starting");
     }
 
-    @FXML
-    private Label label1;
-
     public void initialize() {
-        label1.setText("Blabla");
+        label.setText("Blabla");
     }
-
-
+    */
 }
--- src/main/java/mainpackage/Main.java
+++ src/main/java/mainpackage/Main.java
@@ -8,7 +8,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -22,30 +21,32 @@ public class Main extends Application {
     static Scene scene2;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         URL fxmlFileUrl = getClass().getClassLoader().getResource("sample.fxml");
+        //System.out.println(fxmlFileUrl);
         // URL fxmlFileUrl = getClass().getClassLoader().getResource("Scenebuilder.fxml");
         Parent root = FXMLLoader.load(Objects.requireNonNull(fxmlFileUrl));
         primaryStage.setTitle("Very cool Sudoku");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        primaryStage.setScene(new Scene(root, 600, 600));
+        GridPane gridPane = (GridPane) root;
 
+        //primaryStage.show();
         window = primaryStage;
 
         Label label1 = new Label("Welcome");
-
+        /*
         //Button 1
-        Button button1 = new Button("Go to scene 2");
-        button1.setOnAction (e -> window.setScene(scene2));
+        Button button = new Button("Go to scene 2");
+        button.setOnAction(e -> window.setScene(scene2));
 
         //Layout 1 - children are laid out in vertical column
         StackPane layout1 = new StackPane();
-        layout1.getChildren().addAll(label1, button1);
+        gridPane.getChildren().addAll(label1, button);
         scene1 = new Scene(layout1, 600, 600);
 
         //Button 2
         Button button2 = new Button("go back to scene 1");
-        button2.setOnAction(e -> window.setScene(scene1));
+        button2.setOnAction(e -> window.setScene(gridPane.getScene()));
 
         //Layout 2
         //VBox layout2 = new VBox(20);
@@ -53,38 +54,38 @@ public class Main extends Application {
         GridPane layout2 = new GridPane();
         layout2.getChildren().addAll(button2);
         scene2 = new Scene(layout2, 600, 600);
+        */
 
-
-        window.setScene(scene1);
+        //window.setScene(scene1);
         window.setTitle("Sudoku");
         window.show();
 
 
         Games games = new Games();
 
-        startRound();
+        //startRound();
     }
 
-    private Board puzzleBoard = Games.getPuzzle_board(); // gets modified by user
-    private final Board fixedPuzzleBoard = Games.getPuzzle_board(); // Reset all -> puzzleBoard = fixedPuzzleBoard
+    private Board puzzleBoard = Games.getPuzzle_board();
     private Board solutionBoard = Games.getSolution_board();
 
     private boolean gameFinished = false;
 
-    /***
+    /*
      *  Hauptablauf des Spiels
      */
     public void startRound() {
 
+
         Scanner sc = new Scanner(System.in);
 
         // immer wieder nach input fragen, den eingegebenen input auf dem rätselbrett updaten, nach fehlern prüfen, ...
-        while(!gameFinished){
+        while (!gameFinished) {
             System.out.println("Rätsel: ");
             System.out.println(puzzleBoard.toString());
 
-            System.out.println("Lösung: ");
-            System.out.println(solutionBoard.toString());
+            System.out.println("Lösung: (auskommentiert)");
+            // System.out.println(solutionBoard.toString());
 
 
             // (der Teil wird später über die GUI umgesetzt)
@@ -93,16 +94,13 @@ public class Main extends Application {
             int lineNr = sc.nextInt() - 1;
             System.out.println("Enter column number: ");
             int colNr = sc.nextInt() - 1;
-            System.out.println("Enter the number you want to set at (" + (lineNr+1) + "|" + (colNr+1) + "): ");
+            System.out.println("Enter the number you want to set at (" + lineNr + 1 + "|" + colNr + 1 + "): ");
             int numberToSet = sc.nextInt();
 
-
             puzzleBoard.setValueInBrett(lineNr, colNr, numberToSet);
-
-
-            if(puzzleBoard.equalsBoard(solutionBoard)) gameFinished = true;
         }
-        System.out.println("Congrats, you finished the puzzle!");
+
+        // board.checkWinning();
     }
 
     public static void main(String[] args) {
