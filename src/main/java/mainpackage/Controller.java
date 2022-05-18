6073d0478146e4922e0258d5f58950c0fea2e119
jr132 <jr132@hdm-stuttgart.de>
2022-05-18 15:00:26 +0200

    user can modify numbers on the puzzle board

--- src/main/java/mainpackage/Controller.java
+++ src/main/java/mainpackage/Controller.java
@@ -2,6 +2,7 @@ package mainpackage;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class Controller {
 
@@ -9,4 +10,13 @@ public class Controller {
     private void sayHello(ActionEvent event) {
         System.out.println("Game starting");
     }
+
+    @FXML
+    private Label label1;
+
+    public void initialize() {
+        label1.setText("Blabla");
+    }
+
+
 }
